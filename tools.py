ce3395f89d2113eb58b2883ee962acad2ac380b4
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-03-21 17:19:13 +0300

    Update tools.py

---
+++ tools.py
@@ -0,0 +1,32 @@
+import re
+import textwrap as tw
+from datetime import datetime
+
+
+def remove_chars(s):
+    return re.sub(r'[^0-9a-zA-Zа-яА-Яё]+', ' ', s)
+
+
+def create_passport(number, issue_date, department, code):
+    """Создает строку с данными паспорта"""
+    passport = ''
+    if number or department or code:
+        passport = """паспорт: %s, выдан %s, орган: %s, код: %s""" \
+                   % (number, issue_date, department, code)
+    return tw.dedent(passport)
+
+
+def create_contract(begin_date, end_date):
+    """Создает строку с данными договора с водителем"""
+    contract = ''
+    if begin_date or end_date:
+        contract = """начат: %s, окончание: %s""" \
+                   % (begin_date, end_date)
+    return tw.dedent(contract)
+
+
+def format_date_string(date_string, format='%d.%m.%Y'):
+    """Форматирует строку с датой в формат даты"""
+    old_format = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%S')
+    new_format = old_format.strftime(format)
+    return new_format
