b938f0810fe6153baa5f670495ea8b018e8fb4cd
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-03-22 20:12:57 +0300

    Update google.py

--- google.py
+++ google.py
@@ -17,7 +17,6 @@ def _get_service_creds():
         https://developers.google.com/sheets/api/quickstart/python
     """
     scopes = ['https://www.googleapis.com/auth/spreadsheets']
-    creds = None
     current_directory = os.path.dirname(os.path.realpath(__file__))
     creds_file = os.path.join(current_directory, "creds.json")
     creds = service_account.Credentials. \
@@ -30,7 +29,7 @@ def _get_service_creds():
     return build_connection
 
 
-def create_sheet(title, sheets=['']):
+def create_sheet(title, sheets):
     """
     https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets
     Создание таблицы и листа/листов.
@@ -84,7 +83,7 @@ def get_values(spreadsheet_id, range_name):
     raise HttpError
 
 
-def batch_get_values(spreadsheet_id, range_names=[]):
+def batch_get_values(spreadsheet_id, range_names):
     """
     https://developers.google.com/sheets/api/quickstart/python#step_3_set_up_the_sample
     Пакетный запрос на данные из МАССИВА ДИАПАЗОНОВ
