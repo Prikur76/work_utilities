7b6a290d155db20e42ed0313bb4b58f72f4a7d6b
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-08-09 15:28:59 +0300

    Update main function

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python
-
 import logging
-import os
 import time
 
 import requests
-import schedule
-from dotenv import load_dotenv
+from environs import Env
 from googleapiclient.errors import HttpError
 
 import element as el
+import settings as set
 import spreadsheets as ss
 import yandex as ya
+
+env = Env()
+env.read_env()
 
 logger = logging.getLogger(__name__)
 
@@ -34,6 +35,31 @@ def create_roster_for_report(park_id, api_key, active_drivers):
         .drop_duplicates()
     return sorted_balances
 
+def update_google_sheet(active_cars, active_drivers, park):
+    """
+    Обновляет таблицу с отчетом в гугле
+    """
+    sheets_ids = park['sheets_ids']
+    park_cars = active_cars[
+        active_cars['Region'] == park['region']
+    ].values.tolist()
+
+    park_drivers = []
+    for i in park['yandex']:
+        park_drivers += create_roster_for_report(
+            i['park_id'], i['api_key'], active_drivers
+        ).values.tolist()
+
+    for sheet_id in sheets_ids:
+        ss.batch_clear_values(sheet_id,
+                              ranges=[set.DRIVERS_RANGE_FOR_UPDATE])
+        ss.batch_update_values(sheet_id,
+                               set.DRIVERS_RANGE_FOR_UPDATE,
+                               park_drivers)
+        ss.batch_update_values(sheet_id,
+                               set.CARS_RANGE_FOR_UPDATE,
+                               park_cars)
+
 
 def main():
     logging.basicConfig(
@@ -41,116 +67,55 @@ def main():
         level=logging.INFO
     )
 
-    load_dotenv()
-    user = os.environ.get('ELEMENT_LOGIN')
-    password = os.environ.get('ELEMENT_PASSWORD')
-    drivers_url = os.environ.get('ELEMENT_DRIVERS_URL')
-    cars_url = os.environ.get('ELEMENT_CARS_URL')
-    taxoparks = {
-        'reports': {
-            'park_id': None,
-            'api_key': None,
-            'sheets_ids': [os.environ.get('REPORT_SPREADSHEET_ID')],
-            'region': None,
-        },
-        'moscow': {
-            'park_id': os.environ.get('MSK_PARK_ID'),
-            'api_key': os.environ.get('MSK_X_API_KEY'),
-            'sheets_ids': [os.environ.get('MSK_SPREADSHEET_ID')],
-            'region': os.environ.get('MSK_REGION'),
-        },
-        'ekaterinburg': {
-            'park_id': os.environ.get('EKB_PARK_ID'),
-            'api_key': os.environ.get('EKB_X_API_KEY'),
-            'sheets_ids': [os.environ.get('EKB_SPREADSHEET_ID')],
-            'region': os.environ.get('EKB_REGION')
-        },
-        'yaroslavl': {
-            'park_id': os.environ.get('YAR_PARK_ID'),
-            'api_key': os.environ.get('YAR_X_API_KEY'),
-            'sheets_ids': [
-                os.environ.get('YAR_SPREADSHEET_ID'),
-                os.environ.get('KSTR_SPREADSHEET_ID')
-            ],
-            'region': os.environ.get('YAR_REGION')
-        },
-    }
-    range_for_update = os.environ.get('RANGE_FOR_UPDATE')
-    cars_ranges_for_update = os.environ.get('RANGE_CARS_FOR_UPDATE')
+    element = el.Element(set.USER, set.PASSWORD)
+    reports_sheets_ids = set.REPORTS_SHEETS_IDS
 
     try:
-        element = el.Element(user, password)
-        exclude_roster = [
-            '', 'Комфорт', 'Подключашки 2 %', 'ПОДКЛЮЧАШКА 3%', 'Штатный'
-        ]
         active_drivers = element.fetch_active_drivers(
-            url=drivers_url, conditions_exclude=exclude_roster
-        )
-        cars = element.fetch_active_cars(cars_url)
-        active_cars = cars[
+            url=set.DRIVERS_URL,
+            conditions_exclude=set.EXCLUDE_ROSTER)
+        element_cars = element.fetch_active_cars(set.CARS_URL)
+        active_element_cars = element_cars[
             [
                 'Model', 'Number', 'VIN', 'YearCar', 'MileAge',
                 'Region', 'Status', 'SubStatus', 'Reason',
                 'Comment',
             ]
         ]
+        all_active_cars = active_element_cars.values.tolist()
 
-        for park in taxoparks.values():
-            park_id, api_key, sheets_ids, region = park.values()
-            if park_id and api_key and region:
-                roster = create_roster_for_report(
-                    park_id, api_key, active_drivers
-                )
-                drivers_records = roster.values.tolist()
-                region_filter = (active_cars.Region == region)
-                cars_for_upload = active_cars[region_filter].values.tolist()
-                for sheet_id in sheets_ids:
-                    ss.batch_clear_values(
-                        sheet_id,
-                        ranges=[range_for_update, cars_ranges_for_update]
-                    )
-                    ss.batch_update_values(
-                        sheet_id, range_for_update, drivers_records
-                    )
-                    ss.batch_update_values(
-                        sheet_id, cars_ranges_for_update, cars_for_upload
-                    )
-            else:
-                cars_for_upload = active_cars.values.tolist()
-                for sheet_id in sheets_ids:
-                    ss.batch_clear_values(
-                        sheet_id, ranges=[cars_ranges_for_update]
-                    )
-                    ss.batch_update_values(
-                        sheet_id, cars_ranges_for_update, cars_for_upload
-                    )
+        for sheet_id in reports_sheets_ids:
+
+            ss.batch_clear_values(sheet_id,
+                                  ranges=[set.CARS_RANGE_FOR_UPDATE])
+
+            ss.batch_update_values(sheet_id,
+                                   set.CARS_RANGE_FOR_UPDATE,
+                                   all_active_cars)
+
+        update_google_sheet(active_element_cars,
+                            active_drivers, set.MSK_PARKS)
+        update_google_sheet(active_element_cars,
+                            active_drivers, set.EKAT_PARKS)
+        update_google_sheet(active_element_cars,
+                            active_drivers, set.YAR_PARKS)
 
     except HttpError as ggl_http_err:
-        logger.error(
-            msg=f'Ошибка подключения гугла: {ggl_http_err}',
-            stack_info=False
-        )
+        logger.error(msg=f'Ошибка подключения гугла: {ggl_http_err}',
+                     stack_info=False)
     except requests.exceptions.HTTPError as http_err:
-        logger.error(
-            msg=f'Ошибка запроса: {http_err}',
-            stack_info=False
-        )
+        logger.error(msg=f'Ошибка http-запроса: {http_err}',
+                     stack_info=False)
     except requests.exceptions.ChunkedEncodingError as chunked_err:
-        logger.error(
-            msg=f'Ошибка обработки пакета: {chunked_err}',
-            stack_info=False
-        )
+        logger.error(msg=f'Ошибка обработки пакета: {chunked_err}',
+                     stack_info=False)
     except requests.exceptions.Timeout as timeout_err:
-        logger.error(
-            msg=f'Timeout: {timeout_err}',
-            stack_info=False
-        )
+        logger.error(msg=f'Timeout: {timeout_err}',
+                     stack_info=False)
         time.sleep(300)
-    except requests.exceptions.ConnectionError as connection_err:
-        logger.error(
-            msg=f'Lost HTTP connection: {connection_err}',
-            stack_info=False
-        )
+    except requests.exceptions.ConnectionError as conn_err:
+        logger.error(msg=f'Ошибка HTTP соединения: {conn_err}',
+                     stack_info=False)
         time.sleep(60)
 
 
@@ -158,10 +123,3 @@ if __name__ == '__main__':
     while True:
         main()
         time.sleep(900)
-    # schedule.every().hour.at('14:00').do(main)
-    # schedule.every().hour.at('29:00').do(main)
-    # schedule.every().hour.at('44:00').do(main)
-    # schedule.every().hour.at('59:00').do(main)
-    # while True:
-        # schedule.run_pending()
-        # time.sleep(1)
