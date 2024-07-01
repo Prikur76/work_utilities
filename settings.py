e4fcfc84110010bdcb901928da7a9fff19935870
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2024-07-01 13:39:27 +0300

    Add parks ids for yandex api

--- settings.py
+++ settings.py
@@ -11,6 +11,40 @@ CARS_URL = env.str('ELEMENT_CARS_URL')
 REPORT_ID = env.str('REPORT_SPREADSHEETS_ID')
 RANGE_FOR_UPLOAD = env.str('RANGE_FOR_UPLOAD')
 RANGE_FOR_UPLOAD_DRIVERS = env.str('RANGE_FOR_UPLOAD_DRIVERS')
+RANGE_FOR_SELFEMPLOYED = env.str('RANGE_FOR_SELFEMPLOYED')
 
 EXCLUDE_ROSTER = ['', 'Комфорт', 'Штатный',
                   'Подключашки 2 %', 'ПОДКЛЮЧАШКА 3%']
+
+PARKS = [
+    {
+        'city': 'МОСКВА',
+        'id': env.str('MSK_PARK_ID'),
+        'api_key': env.str('MSK_X_API_KEY')
+    },
+    {
+        'city': 'МОСКВА (СОЦИАЛЬНЫЙ)',
+        'id': env.str('SPECIAL_PARK_ID'),
+        'api_key': env.str('SPECIAL_X_API_KEY')
+    },
+    {
+        'city': 'МОСКВА',
+        'id': env.str('ELS_PARK_ID'),
+        'api_key': env.str('ELS_X_API_KEY')
+    },
+    {
+        'city': 'Екатеринбург',
+        'id': env.str('EKB_PARK_ID'),
+        'api_key': env.str('EKB_X_API_KEY')
+    },
+    {
+        'city': 'Ярославль',
+        'id': env.str('YAR_PARK_ID'),
+        'api_key': env.str('YAR_X_API_KEY')
+    },
+    {
+        'city': 'ЧЕЛЯБИНСК',
+        'id': env.str('CHL_PARK_ID'),
+        'api_key': env.str('CHL_X_API_KEY')
+    }
+]
